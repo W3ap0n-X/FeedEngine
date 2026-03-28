cfb78fbaab7f83ee0976cf7f3ca8834e61583bb2
AndyV <andy@FR3CKL35.localdomain>
2026-03-27 22:28:39 -0600

    fatal error fix 6

--- qck-feed-engine.php
+++ qck-feed-engine.php
@@ -26,6 +26,6 @@ register_deactivation_hook( __FILE__, [Core\Activator::class, 'deactivate'] );
 register_uninstall_hook( __FILE__, [ \Core\Install\Uninstall::class, 'cleanup' ] );
 
 // Ignition
-add_action( 'plugins_loaded', function() {
-    Plugin::instance();
-});
+require plugin_dir_path( __FILE__ ) . 'inc/Core/Plugin.php';
+
+new Plugin();
