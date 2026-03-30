21bf183bc41eb26d7a75b52ee0b22b6b156dd5c7
AndyV <andy@FR3CKL35.localdomain>
2026-03-30 14:35:46 -0600

    Switched to modular options and hooks - fix typo

---
+++ src/Hooks/PublicAssets.php
@@ -0,0 +1,22 @@
+<?php
+namespace Qck\FeedEngine\Hooks;
+use Qck\FeedEngine\Manifest;
+
+class PublicAssets implements HookInterface {
+    public function get_hook(): string { return 'wp_enqueue_scripts'; }
+    public function get_priority(): int { return 10; }
+    public function get_args_count(): int { return 1; }
+
+    public function get_callback(): callable {
+        return function() {
+            wp_enqueue_style( Manifest::PREFIX . '-main', Manifest::url() . 'assets/css/public.css' );
+            wp_enqueue_script( 
+                Manifest::PREFIX . '-main',
+                Manifest::url('assets/js/public.js'), 
+                ['jquery'], // Added jquery as a dependency since your script uses it
+                Manifest::VERSION, 
+                true // Move to footer for better performance
+            );
+        };
+    }
+}
---
+++ src/assets/js/public.js
@@ -0,0 +1,1 @@
+console.log("FEEDENGINE JS LOADED");
