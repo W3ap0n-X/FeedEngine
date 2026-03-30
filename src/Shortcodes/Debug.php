18c27d77fc3a5a83ef853b0dd23df38d2dc23c49
AndyV <andy@FR3CKL35.localdomain>
2026-03-30 15:29:28 -0600

    troubleshoot options rendering

--- src/Shortcodes/Debug.php
+++ src/Shortcodes/Debug.php
@@ -25,7 +25,7 @@ class Debug implements Shortcode {
         $a = shortcode_atts( $this->atts, $atts );
         $output = '';
         if($a['plugin_info'] !== false){
-            $output .= \Qck\FeedEngine\Core\Debug::easydump('Plugin Details', \Qck\FeedEngine\Core\Debug::details());
+            $output .= \Qck\FeedEngine\Core\Debug::easydump('Plugin Details', Manifest::details());
         }
         if(isset($a['message'])){
             $output .= \Qck\FeedEngine\Core\Debug::easydump($a['label'], $a['message']);
