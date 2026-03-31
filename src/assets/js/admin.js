834920f12ab03eb09c80104fa2f1bb37c0ceee38
AndyV <andy@FR3CKL35.localdomain>
2026-03-30 19:56:46 -0600

    troubleshoot options rendering

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -11,7 +11,7 @@ console.log(settings.nonce);
         
         $form.on('submit', function(e) {
             e.preventDefault();
-            let testjson = JSON.serialize($form);
+            let testjson = JSON.stringify($form);
             console.log(testjson);
             // Visual feedback: disable button
             const $submitBtn = $form.find('input[type="submit"], button[type="submit"]');
